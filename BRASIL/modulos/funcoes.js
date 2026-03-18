a4c29349603b2ce2eab45193d9900a3bf7c1c46d
FRANCISCO WALA NORONHA MACEDO <25203678@CFP127ALUNO.LOCAL>
2026-03-18 16:17:52 -0300

    Iniciando Projeto

---
+++ BRASIL/modulos/funcoes.js
@@ -0,0 +1,40 @@
+/********************************************************************************
+ * Objetivo do projeto: Criar funções para realizar buscas dentro de Banco 
+ * de Dados(Simulado por Prof. Marcel)
+ * Autor: Francisco Wala
+ * Data: 18/03/2026
+ * Versão: 1.0
+ *********************************************************************************/
+const arquivo = require('./arquivo')
+
+/**Requisitos do Sistema
+● Criar uma função (getListaDeEstados) que retorna a lista de todos os
+estados do Brasil.
+
+● Criar uma função (getDadosEstado) que retorna as informações referente
+a um estado do Brasil, onde a sigla do estado será o critério de filtro.
+
+● Criar uma função (getCapitalEstado) que retorna as informações
+referente a capital de um estado do Brasil, onde a sigla do estado será o
+critério de filtro.
+
+● Criar uma função (getEstadosRegiao) que retorna as informações
+referente aos estados do Brasil conforme a sua região, onde a região será o
+critério de filtro.
+
+Desafio bem legal, mas esse é mais legal do que
+os outros
+
+● Criar uma função (getCapitalPais) que retorna as informações referente
+aos estados que formam a capital do Brasil.
+
+● Criar uma função (getCidades) que retorna uma lista de cidades, filtrado
+pela sigla do estado.
+
+Bom Divertimento!!! */
+// function getListaDeEstados (){
+
+// }
+
+console.log(arquivo.listaDeEstados.estados)
+
