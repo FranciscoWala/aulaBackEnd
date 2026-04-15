2b7b8bb27ae1d51e59a59c973bbeaaf43ceeccfe
FRANCISCO WALA NORONHA MACEDO <25203678@CFP127ALUNO.LOCAL>
2026-04-15 17:04:12 -0300

    [wix]adiciona algumas funcionalidades e atualização de documentação

---
+++ AULA 06(NEW WORLD(MOISES))/model/DAO/filme/filme.js
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Objetivo: Arquivo responsável pelo CRUD de dados do filme no banco de dados
+ * MySQL
+ * Data: 15/04/26
+ * Autor: Francisco Wala
+ * Versão: 1.0
+ **********************************************************************************/
+
+//Função para inserir novo filme no banco de dados
+const insertFilme = async function (filme) {
+    let sql = ` insert into tbl_filme (
+	nome,
+    sinopse,
+    capa,
+    data_lancamento,
+    duracao,
+    valor,
+    avaliacao
+) values (
+	'${filme.nome}',
+    '${filme.sinopse}',
+    '${filme.capa}',
+    '${filme.data_lancamento}',
+    '${filme.duracao}',
+    '${filme.valor}',
+    '${filme.avaliacao}'
+); `
+
+}
+
+//Função para atualizar um filme existente no banco de dados
+const updateFilme = async function (filme) {
+
+}
+
+//Função para retornar todos os dados de filme do banco de dados
+const selectAllFilme = async function () {
+
+}
+
+//Função para retornar um filme filtrando pelo ID
+const selectByIdFilme = async function (id) {
+
+}
+
+//Função para excluir um filme pelo ID
+const deletefilme = async function (id) {
+
+}
+
+module.exports = {
+    insertFilme,
+    updateFilme,
+    selectAllFilme,
+    selectByIdFilme,
+    deletefilme,
+}
